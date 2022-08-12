1573e3c6c016ddf633defec0c8e7314e444bf0fe
Tasha1003Natasha <0955712824n@gmail.com>
2022-08-12 19:53:44 +0300

    правка home

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,23 +1,10 @@
 import React from 'react';
-import Ratio from 'react-bootstrap/Ratio';
+import '../index.css';
 
 const HomeView = () => {
   return (
-    <div
-      style={{
-        width: '800px',
-        height: 'auto',
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        marginTop: '50px',
-      }}
-    >
-      <Ratio aspectRatio="16x9">
-        <embed
-          type="image/svg+xml"
-          src="http://inspiredlearning21stcclc.weebly.com/uploads/4/3/3/9/43399847/welcome2_orig.jpg"
-        />
-      </Ratio>
+    <div className="home">
+      <h1 className="title-home">Welcome to phonebook</h1>
     </div>
   );
 };
